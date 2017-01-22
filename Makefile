bb919cf29a96047dc4632c8cb82f25aea8bfcfc0
jessecooper <jesse_cooper@codeholics.com>
2017-01-22 13:56:03 -0600

    added some base project files

---
+++ Makefile
@@ -0,0 +1,9 @@
+init:
+	pip install -r requirements.txt
+test:
+	python -m unittest -v tests
+install:
+	pip install .
+clean:
+	find . -iname *.pyc -exec rm -f {} +
+	pip uninstall -y stockapis
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='etrade',
+      version='0.1',
+      description='eTrade API wrappers built on request',
+      author='Jesse Cooper',
+      author_email='jesse_cooper@codeholics.com',
+      url='https://git.aigalactic.com/Spyglass/pyetrade.git',
+      package=['pyetrade']
+     )

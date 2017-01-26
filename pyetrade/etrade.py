54368b2943ffc8d07d47feb713c8110dc9a03ef3
jessecooper <jesse_cooper@codeholics.com>
2017-01-26 11:40:27 -0600

    started base API class

--- pyetrade/etrade.py
+++ pyetrade/etrade.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+'''etrade.py
+   Description: Python interface to the eTrade API
+   TODO:
+    * Authentication
+    * Everything else'''
+
+class ETradeAPI(object):
+    '''ETradeAPI
+       Main API class'''
+    def __init__(self, dev=True):
+        if dev:
+            # eTrade sandbox url
+            self.base_url = 'https://etwssandbox.etrade.com'
+        else:
+            # eTrade prod url
+            self.base_url = 'https://etws.etrade.com'

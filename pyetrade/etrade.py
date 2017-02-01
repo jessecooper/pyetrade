84288b4ced1b88c2309bb9d1347d9e88dadcbe52
jessecooper <jesse_cooper@codeholics.com>
2017-01-31 21:37:11 -0600

    added notes

--- pyetrade/etrade.py
+++ pyetrade/etrade.py
@@ -2,15 +2,23 @@
 '''etrade.py
    Description: Python interface to the eTrade API
    NOTES:
-   Oauth package: rauth (considering)
+   Oauth package: rauth (considering) - rauth package will not work
+                                        lacking ability to add other
+                                        attributes
+                  hashlib - generate HMAC-SHA1
    # Sandbox
    etrade = OAuth1Service(name='etrade',
+                          #oauth_consumer_key='e51a0c96ee21b82c43f320e604ea5f5e',
                           consumer_key='e51a0c96ee21b82c43f320e604ea5f5e',
+                          #oauth_timestamp=time.time(),
+                          #oauth_nonce=nonce(9),
+                          #oauth_signature_method='HMAC-SHA1',
                           consumer_secret='89a4aac688ea391c9bd7facfa40c1453',
                           request_token_url='https://etws.etrade.com/oauth/request_token',
                           access_token_url='https://etws.etrade.com/oauth/access_token',
                           authorize_url='https://us.etrade.com/e/t/etws/authorize',
-                          base_url='https://etwssandbox.etrade.com/')
+                          base_url='https://etwssandbox.etrade.com/',
+                          oauth_callback='oob')
    # Prod
    etrade = OAuth1Service(name='etrade',
                           consumer_key='e51a0c96ee21b82c43f320e604ea5f5e',
@@ -20,6 +28,14 @@
                           authorize_url='https://us.etrade.com/e/t/etws/authorize',
                           base_url='https://etws.etrade.com/accounts/rest/')
 
+   # header
+   header = {'Authorization': r'OAuth realm="",
+                                oauth_callback="oob",
+                                oauth_signature="",
+                                oauth_nonce="LTg2ODUzOTQ5MTEzMTY3MzQwMzE%3D",
+                                oauth_signature_method="HMAC-SHA1",
+                                oauth_consumer_key="e51a0c96ee21b82c43f320e604ea5f5e",
+                                oauth_timestamp="1273254425"'}
    # Request token
    URL
    https://etws.etrade.com/oauth/request_token
@@ -61,6 +77,9 @@
    TODO:
     * Authentication
     * Fix note formatting'''
+
+from os import urandom
+from base64 import b64encode
 
 class ETradeAPI(object):
     '''ETradeAPI

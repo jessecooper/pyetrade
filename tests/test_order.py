38549285be785b6ee52923948aeb8e0d1285873e
jessecooper <jesse_cooper@codeholics.com>
2017-04-01 13:24:41 -0500

    added more tests

--- tests/test_order.py
+++ tests/test_order.py
@@ -27,9 +27,68 @@ class TestETradeOrder(unittest.TestCase):
                                                    marketSession='REGULAR'), "{'accountId': '12345'}") 
         self.assertTrue(MockOAuthSession().post().json.called) 
         self.assertTrue(MockOAuthSession().post.called) 
+        # Test prod buy order equity
+        self.assertEqual(orders.place_equity_order(dev=False,
+                                                   accountId=12345,
+                                                   symbol='ABC', 
+                                                   orderAction='BUY',
+                                                   clientOrderId='1a2b3c',
+                                                   priceType='MARKET',
+                                                   quantity=100,
+                                                   orderTerm='GOOD_UNTIL_CANCEL',
+                                                   marketSession='REGULAR'), "{'accountId': '12345'}") 
+        self.assertTrue(MockOAuthSession().post().json.called) 
+        self.assertTrue(MockOAuthSession().post.called)
+        # Test prod buy order equity
+        self.assertEqual(orders.place_equity_order(dev=False,
+                                                   resp_format='text',
+                                                   accountId=12345,
+                                                   symbol='ABC', 
+                                                   orderAction='BUY',
+                                                   clientOrderId='1a2b3c',
+                                                   priceType='MARKET',
+                                                   quantity=100,
+                                                   orderTerm='GOOD_UNTIL_CANCEL',
+                                                   marketSession='REGULAR'), "<xml> returns </xml>") 
+        self.assertTrue(MockOAuthSession().post().json.called) 
+        self.assertTrue(MockOAuthSession().post.called)
+    @patch('pyetrade.order.OAuth1Session')
+    def test_place_equity_order_exception(self, MockOAuthSession):
+        orders = order.ETradeOrder('abc123', 'xyz123', 'abctoken', 'xyzsecret')
+
         # Test exception class
         with self.assertRaises(etrade_exception.OrderException):
             orders.place_equity_order()
+        # Test STOP
+        with self.assertRaises(etrade_exception.OrderException):
+            orders.place_equity_order(accountId=12345,
+                                      symbol='ABC', 
+                                      orderAction='BUY',
+                                      clientOrderId='1a2b3c',
+                                      priceType='STOP',
+                                      quantity=100,
+                                      orderTerm='GOOD_UNTIL_CANCEL',
+                                      marketSession='REGULAR')
+        #Test LIMIT
+        with self.assertRaises(etrade_exception.OrderException):
+            orders.place_equity_order(accountId=12345,
+                                      symbol='ABC', 
+                                      orderAction='BUY',
+                                      clientOrderId='1a2b3c',
+                                      priceType='LIMIT',
+                                      quantity=100,
+                                      orderTerm='GOOD_UNTIL_CANCEL',
+                                      marketSession='REGULAR')
+        #Test STOP_LIMIT
+        with self.assertRaises(etrade_exception.OrderException):
+            orders.place_equity_order(accountId=12345,
+                                      symbol='ABC', 
+                                      orderAction='BUY',
+                                      clientOrderId='1a2b3c',
+                                      priceType='STOP_LIMIT',
+                                      quantity=100,
+                                      orderTerm='GOOD_UNTIL_CANCEL',
+                                      marketSession='REGULAR')
         # Test Prod JSON
         #self.assertEqual(orders.place_equity_order(dev=False), "{'account': 'abc123'}") 
         # Test Dev XML

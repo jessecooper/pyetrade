e7b7e667b8088f54ed397fdd8353f61413a75967
Jesse Cooper <jesse_cooper@codeholics.com>
2017-06-15 07:36:42 -0500

    added classifier and platform meta data

--- setup.py
+++ setup.py
@@ -8,13 +8,31 @@ with open('requirements.txt') as requirements:
 
 setup(name='pyetrade',
       version='0.1',
-      description='eTrade API wrappers built on requests-oauth',
+      description='eTrade API wrappers',
       author='Jesse Cooper',
       author_email='jesse_cooper@codeholics.com',
       url='https://github.com/jessecooper/pyetrade',
       license='GPLv3',
       packages=['pyetrade'],
       package_dir={'pyetrade':'pyetrade/'},
-      install_requires = req,
-      keywords = ['etrade', 'pyetrade', 'stocks']
+      install_requires=req,
+      platforms=['any'],
+      keywords=['etrade', 'pyetrade', 'stocks'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries'
+          ]
      )

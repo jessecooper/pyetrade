4b329fc1b34efa05d6ab612c043475fcaac11df4
Jesse Cooper <jesse_cooper@codeholics.com>
2017-06-15 14:15:43 -0500

    fixed package issue and bump

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('requirements.txt') as requirements:
     req = [i.strip() for i in requirements]
 
 setup(name='pyetrade',
-      version='0.1.2',
+      version='0.1.3',
       description='eTrade API wrappers',
       author='Jesse Cooper',
       author_email='jesse_cooper@codeholics.com',
       url='https://github.com/jessecooper/pyetrade',
       license='GPLv3',
       packages=['pyetrade'],
-      package_dir={'pyetrade':'pyetrade/'},
+      package_dir={'pyetrade':'pyetrade'},
       install_requires=req,
       platforms=['any'],
       keywords=['etrade', 'pyetrade', 'stocks'],

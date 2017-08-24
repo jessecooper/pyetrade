1435cbd01f2ae0c2b52c516a7ad509d616025cf6
vesc <jesse_cooper@codeholics.com>
2017-08-24 20:48:34 +0000

    added market.py get_quote and tests

--- pyetrade/etrade_exception.py
+++ pyetrade/etrade_exception.py
@@ -7,3 +7,11 @@ class OrderException(Exception):
 
     def __str__(self):
         return 'Missing required paramiters'
+
+class MarketQuoteException(Exception):
+    def __init__(self, explanation=None, params=None):
+        self.required = params
+        self.args = (explanation, params, )
+
+    def __str__(self):
+        return 'Symbole max exceeded limit 25'
---
+++ tests/test_market.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+'''pyetrade market unit tests
+   TODO:
+       * lint'''
+
+import string
+import random
+import unittest
+from unittest.mock import patch
+from pyetrade import market, etrade_exception
+
+class TestETradeMarket(unittest.TestCase):
+    '''TestEtradeMarket Unit Test'''
+    # Mock out OAuth1Session
+    @patch('pyetrade.market.OAuth1Session')
+    def test_get_quote(self, MockOAuthSession):
+        '''test_get_quote(MockOAuthSession) -> None
+           param: MockOAuthSession
+           type: mock.MagicMock
+           description: MagicMock of OAuth1Session'''
+        # Set Mock returns
+        MockOAuthSession().get().json.return_value = "{'BAC': '32.10'}"
+        MockOAuthSession().get().text.return_value = r'<xml> returns </xml>'
+        mark = market.ETradeMarket('abc123', 'xyz123', 'abctoken', 'xyzsecret')
+        # Test Dev Get Quote
+        self.assertEqual(mark.get_quote('BAC'), "{'BAC': '32.10'}")
+        self.assertTrue(MockOAuthSession().get().json.called)
+        self.assertTrue(MockOAuthSession().get.called)
+        # Test prod Get Qoute
+        self.assertEqual(mark.get_quote('BAC', dev=False), "{'BAC': '32.10'}")
+        self.assertTrue(MockOAuthSession().get().json.called)
+        self.assertTrue(MockOAuthSession().get.called)
+        # Test prod Get Qoute xml
+        self.assertEqual(mark.get_quote(
+                'BAC', resp_format='xml', dev=False),
+                r"<xml> returns </xml>"
+            )
+        self.assertTrue(MockOAuthSession().get().text.called)
+        self.assertTrue(MockOAuthSession().get.called)
+
+    @patch('pyetrade.market.OAuth1Session')
+    def test_get_quote_exception(self, MockOAuthSession):
+        '''test_get_quote(MockOAuthSession) -> None
+           param: MockOAuthSession
+           type: mock.MagicMock
+           description: MagicMock of OAuth1Session'''
+        # Generate symboles
+        sym = [''.join(random.choice(string.ascii_uppercase) \
+               for _ in range(3)) for _ in range(30)]
+        mark = market.ETradeMarket('abc123', 'xyz123', 'abctoken', 'xyzsecret')
+
+        # Test exception class
+        with self.assertRaises(etrade_exception.MarketQuoteException):
+            mark.get_quote(*sym)
+        

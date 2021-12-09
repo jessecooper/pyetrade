c7a02e2385fb6bbbf3438beb4c29f5aeb8c225c3
Jesse Cooper <jesse_cooper@codeholics.com>
2021-12-09 14:05:37 -0600

    Bump version: 1.3.2 → 1.3.3

--- pyetrade/__init__.py
+++ pyetrade/__init__.py
@@ -3,7 +3,7 @@
 __ALL__ = ["authorization", "accounts"]
 
 __title__ = "pyetrade"
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 __author__ = "Jesse Cooper"
 
 from . import authorization  # noqa: F401

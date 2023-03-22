8943dc7fcbd674c54685905c5f0ce1b604969388
Jesse Cooper <jesse_cooper@codeholics.com>
2023-03-22 08:14:22 -0500

    Bump version: 1.3.6 → 1.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = "1.3.6"
+__version__ = "1.4.0"
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file

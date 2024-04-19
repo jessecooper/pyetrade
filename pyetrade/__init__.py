cd2d9fd5d7cf982522afa6cb19a7df3e6f3ac809
Jesse Cooper <jesse_cooper@codeholics.com>
2024-04-19 15:39:58 -0500

    Bump version: 2.0.1 → 2.1.0

--- pyetrade/__init__.py
+++ pyetrade/__init__.py
@@ -1,5 +1,5 @@
 """Init for pyetrade module  """
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 from . import authorization  # noqa: F401
 from .authorization import ETradeOAuth, ETradeAccessManager  # noqa: F401
